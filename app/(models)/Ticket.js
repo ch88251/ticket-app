27e97074b450188f41136dfab1149a1030583a41
Charles Hayes <cfhayes16@gmail.com>
2024-10-07 23:21:47 -0600

    Need to setup mongodb still

--- app/(models)/Ticket.js
+++ app/(models)/Ticket.js
@@ -1,8 +1,3 @@
-import mongoose, { Schema } from "mongoose";
-
-mongoose.connect(process.env.MONGODB_URI);
-mongoose.Promise = global.Promise;
-
 const ticketSchema = new Schema(
   {
     title: String,
@@ -17,7 +12,5 @@ const ticketSchema = new Schema(
     timestamps: true,
   }
 );
-
-const Ticket = mongoose.models.Ticket || mongoose.model("Ticket".ticketSchema);
 
 export default Ticket;
